76d3c1d759ab391c33e64a5da96635f91c711828
chenjian <chenjian@caiqr.com>
2020-02-15 22:40:17 +0800

    创建网关EnableDiscoveryClient，能够通过微服务转发请求

---
+++ gateway/src/main/java/org/twelveolympians/zeus/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package org.twelveolympians.zeus.gateway;
+
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@EnableFeignClients(basePackages = "com.springboot.cloud.auth.client")
+//@EnableCircuitBreaker
+//@EnableMethodCache(basePackages = "org.twelveolympians.zeus")
+//@EnableCreateCacheAnnotation
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
