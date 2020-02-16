893537ffb41411eca8eb843e9ce6d1c1a46d3f6f
chenjian <chenjian@caiqr.com>
2020-02-16 11:08:39 +0800

    能够启动gateway

--- gateway/src/main/java/org/twelveolympians/zeus/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/twelveolympians/zeus/gateway/GatewayApplication.java
@@ -1,7 +1,5 @@
 package org.twelveolympians.zeus.gateway;
 
-import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
-import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -11,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 //@EnableFeignClients(basePackages = "com.springboot.cloud.auth.client")
-//@EnableCircuitBreaker
+@EnableCircuitBreaker
 //@EnableMethodCache(basePackages = "org.twelveolympians.zeus")
 //@EnableCreateCacheAnnotation
 public class GatewayApplication {
