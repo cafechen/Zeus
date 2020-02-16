378194667a0981075b681afabe3c728f1d3539c5
chenjian <chenjian@caiqr.com>
2020-02-16 12:32:54 +0800

    提交auth-client feign

--- business/auth/src/main/java/org/twelveolympians/zeus/auth/exception/GlobalExceptionHandlerAdvice.java
+++
@@ -1,9 +0,0 @@
-package org.twelveolympians.zeus.auth.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.twelveolympians.zeus.common.web.exception.DefaultGlobalExceptionHandlerAdvice;
-
-@RestControllerAdvice
-public class GlobalExceptionHandlerAdvice extends DefaultGlobalExceptionHandlerAdvice {
-
-}
