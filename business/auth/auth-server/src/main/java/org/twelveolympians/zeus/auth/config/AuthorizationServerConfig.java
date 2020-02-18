7a3fb76fdd150588030eae7ffe9ae326987b75f2
chenjian <chenjian@caiqr.com>
2020-02-18 14:27:05 +0800

    从数据库中读取clients

--- business/auth/auth-server/src/main/java/org/twelveolympians/zeus/auth/config/AuthorizationServerConfig.java
+++ business/auth/auth-server/src/main/java/org/twelveolympians/zeus/auth/config/AuthorizationServerConfig.java
@@ -13,9 +13,15 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @Configuration
@@ -30,6 +36,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Qualifier("customUserDetailsService")
     UserDetailsService userDetailsService;
 
+    @Qualifier("dataSource")
+    @Autowired
+    DataSource dataSource;
+
     /**
      * jwt 对称加密密钥
      */
@@ -39,10 +49,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         // 配置两个客户端,一个用于password认证 一个用于client认证
-        clients.inMemory().withClient("zeus_client")
+        /*clients.inMemory().withClient("zeus_client")
                 .authorizedGrantTypes("client_credentials", "password", "refresh_token")
                 .scopes("read")
-                .secret("$2a$10$2szDKjvKHJCWE6YQNznogOeQF3USZHmCYj1fG7YbfK.vnTgNKLzri") ;
+                .secret("$2a$10$2szDKjvKHJCWE6YQNznogOeQF3USZHmCYj1fG7YbfK.vnTgNKLzri")
+                .accessTokenValiditySeconds(3600);*/
+        clients.jdbc(dataSource);
     }
 
     @Override
@@ -61,6 +73,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         oauthServer.allowFormAuthenticationForClients();
         oauthServer.tokenKeyAccess("isAuthenticated()")
                 .checkTokenAccess("permitAll()");
+    }
+
+    /**
+     * 授权信息持久化实现
+     *
+     * @return JdbcApprovalStore
+     */
+    @Bean
+    public ApprovalStore approvalStore() {
+        return new JdbcApprovalStore(dataSource);
+    }
+
+    /**
+     * 授权码模式持久化授权码code
+     *
+     * @return JdbcAuthorizationCodeServices
+     */
+    @Bean
+    protected AuthorizationCodeServices authorizationCodeServices() {
+        // 授权码存储等处理方式类，使用jdbc，操作oauth_code表
+        return new JdbcAuthorizationCodeServices(dataSource);
     }
 
     /**
